45be26f51e0f0795d31d82f172f69f209ee7676c
II-EMMA <emma.ahmedera@gmail.com>
2025-10-20 17:00:36 +0300

    routing right

--- src/app/api/purchase/route.js
+++ src/app/api/purchase/route.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth/authOptions";
 import User from "@/models/User";
 import Purchase from "@/models/Purchase";
 
--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth/authOptions";
 import User from "@/models/User";
 
 const connectDB = async () => {
